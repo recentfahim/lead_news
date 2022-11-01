d8b989cad426d0991f23a74410825259dde1ced6
fahim.ahmed <fahim.ahmed@japantravel.com>
2022-11-02 01:29:09 +0600

    added news list view and show it to html

---
+++ news/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import NewsListView
+
+
+urlpatterns = [
+    path('news/', NewsListView.as_view(), name='news_list'),
+]
--- news/views.py
+++ news/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import News
 
-# Create your views here.
+
+class NewsListView(ListView):
+    queryset = News.objects.all()[:20]
+    context_object_name = 'news_list'
+    template_name = 'news_list.html'
