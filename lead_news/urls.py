701fb27a3bb4ecf864484886aac7201385a708be
fahim.ahmed <fahim.ahmed@japantravel.com>
2022-11-03 00:54:04 +0600

    add news card, and fix static files loading issue

--- lead_news/urls.py
+++ lead_news/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('news.urls.web')),
     path('api/v1/', include('news.urls.api'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
